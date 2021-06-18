338522cf9010a8122048517931486a9da08f3952
Alex White <alexlloydwhite@gmail.com>
2021-06-18 15:37:10 -0500

    first sql query to get account information

--- server/routes/account.router.js
+++ server/routes/account.router.js
@@ -4,7 +4,19 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 router.get('/', (req, res) => {
-  res.send('Hello?');
+  const sqlText = `SELECT account.id, account.name, SUM(amount) FROM account
+  JOIN register on account.id = register.acct_id 
+  GROUP BY register.acct_id, account.id;`
+
+  pool.query(sqlText)
+    .then(result => {
+    res.send(result.rows);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.sendStatus(500);
+  });
+
 })
 
 module.exports = router;
