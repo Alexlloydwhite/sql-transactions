a6d1525ff0dbb4a3e989dad76b668f3054bed832
Alex White <alexlloydwhite@gmail.com>
2021-06-18 15:52:15 -0500

    adding await to post account transaction

--- server/routes/account.router.js
+++ server/routes/account.router.js
@@ -36,11 +36,19 @@ router.post('/transfer', (req,res) => {
   const connection = await pool.connection();
 
   try {
-    
+    await connection.query('BEGIN;');
+    const sqlText = 
+    `INSERT INTO register (acct_id, amount) 
+    VALUES ($1, $2);`;
+    await connection.query(sqlText, [fromAccount, -amount]);
+    await connection.query(sqlText, [toAccount, amount]);
+    await connection.query(`COMMIT;`);
   } catch (error) {
-    console.log(`error transferring money!`,error);
+    await connection.query('ROLLBACK;');
+    console.log(`error transferring money!`, error);
     res.sendStatus(500);
-  } finally { // always runs - wether there is an error or not
+  } finally { 
+    // always runs - wether there is an error or not
     // Cleanup - return the database connection to the pool
     // THIS IS SUPER IMPORTANT
     connection.release();
